05ab320707991832ca5a9f91a24afbd51aec4a8a
RafaelBadaro <rafabadaro.15@gmail.com>
2024-06-07 12:36:21 -0300

    changing to token instead of code

--- routes/login.js
+++ routes/login.js
@@ -6,27 +6,27 @@ const client_id = '43e1970597df4433bdd2bd66f10c9029';
 var redirect_uri = 'http:localhost:3000/callback';
 
 function generateRandomString(length) {
-    let text = '';
-    const possible = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789';
+  let text = '';
+  const possible = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789';
 
-    for (let i = 0; i < length; i++) {
-        text += possible.charAt(Math.floor(Math.random() * possible.length));
-    }
-    return text;
+  for (let i = 0; i < length; i++) {
+    text += possible.charAt(Math.floor(Math.random() * possible.length));
+  }
+  return text;
 }
 
 router.get('/', function (req, res, next) {
-    var state = generateRandomString(16);
-    var scope = 'user-read-private user-read-email';
-  
-    res.redirect('https://accounts.spotify.com/authorize?' +
-      querystring.stringify({
-        response_type: 'code',
-        client_id: client_id,
-        scope: scope,
-        redirect_uri: redirect_uri,
-        state: state
-      }));
+  var state = generateRandomString(16);
+  var scope = 'user-read-private user-read-email';
+
+  res.redirect('https://accounts.spotify.com/authorize?' +
+    querystring.stringify({
+      response_type: 'token',
+      client_id: client_id,
+      scope: scope,
+      redirect_uri: redirect_uri,
+      state: state
+    }));
 });
 
 module.exports = router;
